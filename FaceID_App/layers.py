2aa11eb27f653ba9a4db800aac72798f10ee6e6f
Debayan <130232920+DebayanAcharyya@users.noreply.github.com>
2024-02-18 17:12:34 +0530

    Initial Commit

---
+++ FaceID_App/layers.py
@@ -0,0 +1,19 @@
+# Custom L1 Distance layer module
+# WHY DO WE NEED THIS : Its needed to load the custom model
+# Import deorflow as tf
+
+import tensorflow as tf
+from tensorflow.keras.layers import Layer
+
+# Custom L1 Distance Layer from Jupyter
+
+class L1Dist(Layer):
+    def __init__(self, **kwargs):
+        super().__init__()
+
+    def call(self, input_embedding, validation_embedding):
+        return tf.math.abs(input_embedding-validation_embedding)
+    
+    
+
+    
